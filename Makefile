f99a51f6bf7ff68c59cc18cdabcd614031ab990d
naens <naens@gmx.com>
2018-06-06 18:52:14 +0200

    palin:update

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: hello test1
+all: hello test1 palin
 
 hello: hello.asm
 	@nasm -O0 -g -F dwarf -f elf hello.asm
@@ -8,10 +8,13 @@ test1: test1.asm
 	@nasm -O0 -g -F dwarf -f elf test1.asm
 	@ld -m elf_i386 -o test1 test1.o
 
+palin: palin.asm
+	@nasm -O0 -g -F dwarf -f elf palin.asm
+	@ld -m elf_i386 -o palin palin.o
+
 export:
 	@./incl.sh test1.asm > test1-all.asm
-	@nasm -O0 -g -F dwarf -f elf test1-all.asm
-	@ld -m elf_i386 -o test1 test1-all.o
+	@./incl.sh palin.asm > palin-all.asm
 
 clean:
 	rm -f *.o test1-all.asm hello test1
--- palin.asm
+++ palin.asm
@@ -3,7 +3,8 @@ global _start
 	section	.data
 
 	section	.bss
-outstr	resb	100h		; buffer for output string
+outstr	resb	100h			; buffer for output string
+cnt	resd	1			; counter
 
 	section	.text
 
@@ -13,28 +14,28 @@ _start:
 	mov	esi, eax
 	call	str_tonum
 
-	mov	ecx, eax	; counter of numbers in ecx
-	cmp	ecx, 0
+	mov	[cnt], eax		; counter of numbers in ecx
+	cmp	dword [cnt], 0
 	je	exit
 .lp:
 	; read string, compute length
 	call	cons_readln
-	mov	ebx, eax	; save the string address in ebx
-	call	str_length	; eax: length
+	mov	ebx, eax		; string address in ebx
+	call	str_length		; eax: length
 
 	; call palin
-	mov	esi, ebx
+	mov	esi, ebx		; esi: input string
 	; string length already in eax
-	mov	edi, outstr
-	call	palin		; write to outstr, len in eax
+	mov	edi, outstr		; edi: output string
+	call	palin			; write to outstr, len in eax
 
 	; print output string
 	mov	esi, eax
-	mov	[outstr + esi], 0	; set outstr[length] = 0
-	mov	eax, out_str
+	mov	byte [outstr + esi], 0	; set outstr[length] = 0
+	mov	eax, outstr
 	call	cons_prstr
 	
-	dec	ecx
+	dec	dword [cnt]
 	jnz	.lp
 
 exit:
@@ -50,10 +51,127 @@ exit:
 ;;     eax - length of the input string
 ;; Returns:
 ;;     eax - length of the output string
+; Pseudocode:
+;     0. make a copy at destination address
+;     for i := 0, i < length, i := i + 1
+;         dest[i] := src[i]
+;     1. compare
+;     i := 0
+;     j := length - 1
+;     while i < length/2
+;         if [i] > [j] then
+;             goto incleft
+;         if [i] < [j] then
+;             goto copylr
+;         i := i + 1
+;         j := j - 1
+;incleft:
+;     2. increase left (only when left <= right)
+;     i := (length - 1) / 2
+;     while i > 0 and [i] = '9' do
+;         [i] = '0'
+;         i := i - 1		; exit i = 0 OR [i] <> '9'
+;     if [i] = '9' then		; can still happen (if i = 0)
+;         length := length + 1
+;         [0] := '1'
+;         if length mod 2 = 1 then
+;             [length div 2] := '0'
+;     else			; [i] <> '9' => can increment
+;        [i] := [i] + 1
+;copylr:
+;     3. copy left to right
+;     i := 0
+;     j := length - 1
+;     while i < length/2
+;         [j] := [i]
+;         i := i + 1
+;         j := j - 1
+	section .bss
+length	resd	1
+src	resd	1
+dest	resd	1
+
+	section .text
 palin:
-	;TODO
+; 0: initialize variables and copy source string to destination
+	mov	[length], eax
+	mov	[src], esi
+	mov	[dest], edi
+
+	mov	ecx, eax
+	call	str_copy
+	mov	ebx, dest	; ebx = address of the destination
+				;       for the rest of the function
+
+; 1: compare left to right
+	mov	esi, 0
+	mov	edi, [length]
+	dec	edi
+
+	mov	ecx, [length]
+	shr	ecx, 1
+
+.w1lp:
+	cmp	esi, ecx
+	jge	.w1e
+	mov	al, [ebx + esi]
+	cmp	al, [ebx + edi]
+	ja	.incleft
+	jb	.copylr
+.w1e:
+
+.incleft:
+; 2: increment the left part starting from the middle
+	mov	esi, [length]
+	dec	esi
+	shr	esi, 1
+
+.w2lp:
+	cmp	esi, 0
+	jbe	.w2e
+	cmp	byte [ebx + esi], '9'
+	jne	.w2e
+	mov	byte [ebx + esi], '0'
+	dec	esi
+	jmp	.w2lp
+.w2e:
+
+	; if [i] = '9' then ...
+	cmp	byte [ebx + esi], '9'
+	jne	.else
+
+	inc	dword [length]
+	mov	byte [ebx], '1'
+
+	; if length mod 2 = 1 then ...
+	test	dword [length], 1
+	jz	.fi
+	mov	esi, [length]
+	shr	esi, 1
+	mov	byte [ebx + esi], '0'
+	jmp	.fi
+.else:
+	inc	dword [ebx + esi]
+.fi:
+
+; 3: copy left to right (mirror)
+.copylr:
+	mov	esi, 0
+	mov	edi, [length]
+	dec	dword edi
+
+.w3lp:
+	cmp	esi, ecx
+	jnb	.w3e
+	mov	al, [ebx + esi]
+	mov	byte [ebx + edi], al
+	inc	esi
+	dec	edi
+	jmp	.w3lp
+.w3e:
 	ret
 
 %include "linux.def"
 %include "str.asm"
 %include "cons.asm"
+
